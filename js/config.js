f4b6253cc5ba5bbe0543378bb7b2ea90e2874156
stevofranchise <stevofranchise24@gmail.com>
2025-02-20 22:59:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_A6lidq1PO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '302os06caf8inp9h6qfamdugbt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
