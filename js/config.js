5588901be2bc1c1c8817b0e935cb28b051709558
Stevo Franchise <stevofranchise24@gmail.com>
2025-09-30 19:25:16 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_wmvb5m9x5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5s50hl5uq5d0bqp3r2993mbmsl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_T33hcBzri', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4osu1uuvicuc8e1jn8bvvt0f34', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://pz2snag8cj.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
