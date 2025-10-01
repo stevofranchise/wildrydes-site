c3a1d5b1d1ea56ed3c4bc525b8204ee49cfb63da
Stevo Franchise <stevofranchise24@gmail.com>
2025-09-30 21:37:12 -0400

    Update esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [-74.00, 45.71],
+            center: [-74.00, 41.71],
             container: 'map',
             map: map,
             zoom: 12
@@ -120,6 +120,7 @@ WildRydes.map = WildRydes.map || {};
         };
     });
 }(jQuery));
+
 
 
 
